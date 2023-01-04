f0173ae109142638e4bcbb9784d81fbf22450f2d
Kristian Gaylord <krlgaylord@gmail.com>
2023-01-03 22:58:51 -0500

    readme and start script

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# eulith-in-a-box
-This repo contains a standalone docker container that can be used to test your integration with Eulith. Our production application compile in and paired with a local Anvil development chain. You can also trivially fork Goerli or Ethereum Mainnet.
+# Eulith In a Box
+This repo contains a standalone docker image (`eulith-devrpc`) 
+that can be used to test your integration with Eulith.
+
+When you run `eulith-devrpc`, you get:
+1. A local Anvil development chain
+2. The full Eulith webserver
+3. Trivial forking support for Ethereum Mainnet and Goerli
+4. Pre-funded wallets with ETH and WETH
+
+🚨🚨 NOTE: Using this service WILL NOT consume any real ETH or GoerliETH. 
+That's the whole point of having a dev binary like this. 🚨🚨
+
+# Run
+`./start.sh`
+
+If you would like to force an update of the docker image, run `./start.sh -r`
+
+If you need help, run `./start.sh -h` or get in touch with us
+
+### Forking (ETH Mainnet & Goerli)
+You can trivially (at least we think so) fork Mainnet and Goerli.
+
+When you run the start script, just add:
+
+1. `-c` : chain id (1 for ETH Mainnet, 5 for Goerli)
+2. `-b` : block number
+
+Examples:
+
+`./start.sh -c 1 -b 16327616` fork Mainnet, block number 16327616
+
+`./start.sh -c 5 -b 16327616` fork Goerli, block number 16327616
+
+When you run the image, you'll need to wait until you see `Starting server on port: 7777`.
+It may take a few seconds for everything to spin up.
+
+# Shutdown
+If you ran the docker image with the `-it` as suggested, you can just
+`Crtl-C` the process.
+
+Otherwise, you'll have to run `docker container ls`,
+find the container ID, and kill with `docker container stop <CONTAINER_ID>`
+
+🚨🚨 NOTE: This service DOES NOT preserve state. If you 
+shut down the process and restart, YOUR LOCAL ACTIONS WILL 
+NOT PERSIST 🚨🚨
---
+++ start.sh
@@ -0,0 +1,71 @@
+TARFILE="$(find . -type f -iname "eulith-devrpc_*.tar")"
+RELOAD="f"
+FORK_CHAIN_ID="0"
+FORK_BLOCK_ID="0"
+FORK_ARGS=""
+
+function print_usage() {
+    echo "Use this script to start Eulith DEV-RPC services. Example: ./start.sh"
+    echo "If you run this script with no arguments, it will check whether the docker image already exists"
+    echo "If it exists, it will load the existing image. Otherwise, it will import anew."
+    echo "If you would like to force an image reload, just pass the -r argument. Example: ./start.sh -r"
+}
+
+while getopts 'hrb:c:' flag; do
+  case "${flag}" in
+    r) RELOAD='t' ;;
+    h) print_usage
+       exit 0 ;;
+    b) FORK_BLOCK_ID="${OPTARG}" ;;
+    c) FORK_CHAIN_ID="${OPTARG}" ;;
+  esac
+done
+
+docker image inspect eulith-devrpc > /dev/null 2>&1
+if [[ $? != 0  || $RELOAD == "t" ]]
+then
+  echo "Loading Docker image from $TARFILE... this may take a few moments"
+  docker load < $TARFILE
+  if [[ $? != 0 ]]
+  then
+    echo "Failed to load image"
+    exit 1
+  fi
+  echo "Done loading"
+fi
+
+if [[ $FORK_CHAIN_ID != "0" ]]
+then
+  if [[ $FORK_CHAIN_ID != "5" && $FORK_CHAIN_ID != "1" ]]
+  then
+    echo "Only chain id 1 (mainnet) or 5 (goerli) is supported for forking"
+    exit 1
+  fi
+
+  FORK_ARGS="-c $FORK_CHAIN_ID"
+fi
+
+if [[ $FORK_BLOCK_ID != "0" ]]
+then
+  FORK_ARGS="$FORK_ARGS -b $FORK_BLOCK_ID"
+fi
+
+if [[ $FORK_ARGS != "" ]]
+then
+  docker run -p 7777:7777 eulith-devrpc $FORK_ARGS > /dev/null 2>&1&
+else
+  docker run -p 7777:7777 eulith-devrpc > /dev/null 2>&1&
+fi
+
+echo "Starting server, please wait ..."
+sleep 20
+
+if [[ $? != 0 ]]
+then
+  echo "Server start failed"
+fi
+
+echo "Started Eulith DEV-RPC server on http://localhost:7777"
+echo "Username: libtest"
+echo "Password: test"
+echo "Fork arguments (-c=chain, -b=block) $FORK_ARGS"
